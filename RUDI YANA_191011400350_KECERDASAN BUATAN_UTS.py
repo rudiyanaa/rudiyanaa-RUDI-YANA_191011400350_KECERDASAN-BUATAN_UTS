b51dc1906e82fa8c88d42d05dbd8a138dafe84f5
rudiyanaa <104075317+rudiyanaa@users.noreply.github.com>
2022-04-20 22:31:07 +0700

    Add files via upload

---
+++ RUDI YANA_191011400350_KECERDASAN BUATAN_UTS.py
@@ -0,0 +1,16 @@
+# Nama  : RUDI YANA
+# Nim   : 191011400350
+# Kelas : 06 TPLM 003
+
+nilai = 90
+
+print("Nilai anda adalah:", nilai, "\n")
+
+if nilai >= 90:
+  print("Anda Mendapatkan Nilai A | Lulus")
+elif nilai >= 80:
+  print("Anda Mendapatkan Nilai B | Lulus")
+elif nilai >= 70:
+    print("Anda Mendapatkan Nilai C | Lulus")
+else:
+    print(" tidak lulus!!!")
